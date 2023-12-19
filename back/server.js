dc1b74238447a488b5cea68b05ce700ad00cb975
GabrielToth <gabt71@gmail.com>
2023-12-19 14:06:38 +0100

    init update

--- back/server.js
+++ back/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 
-const port = normalizePort(process.env.PORT || '3001');
+const port = process.env.PORT || '3001';
+
 app.set('port', port);
 
 app.get('/', function (req, res) {
