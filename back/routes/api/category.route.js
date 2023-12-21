1008c62c072b24493badac18e2816f8b4e64e27e
Vilguax <axel.pelassa@hotmail.fr>
2023-12-21 16:02:24 +0100

    desactivation protection

--- back/routes/api/category.route.js
+++ back/routes/api/category.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 
-router.get('/protected-route', authMiddleware, (req, res) => {
+/*router.get('/protected-route', authMiddleware, (req, res) => {
   res.send('Cette route est protégée et vous êtes authentifié.');
-});
+});*/
 
 const { getCategories } = require('../../controllers/category.controller');
 
