cee7bb443c0da82c4da53b36c0fd309a0365f0ca
Vilguax <axel.pelassa@hotmail.fr>
2023-12-21 15:54:48 +0100

    ajout authMiddleware et JWT

--- back/routes/api/category.route.js
+++ back/routes/api/category.route.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+
+router.get('/protected-route', authMiddleware, (req, res) => {
+  res.send('Cette route est protégée et vous êtes authentifié.');
+});
 
 const { getCategories } = require('../../controllers/category.controller');
 
