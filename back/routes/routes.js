c48c2df816a319e262ed3fed5b25510dea6ef4b4
Vilguax <axel.pelassa@hotmail.fr>
2023-12-21 14:35:07 +0100

    .

--- back/routes/routes.js
+++ back/routes/routes.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express()
+const router = express.Router();
+const dotenv = require('dotenv').config();
 
+router.get('/categories', async (req, res) => {
+    const apiKey = process.env.API_KEY;
+    console.log(apiKey);
+    
+    const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=fr-FR`;
+  
+    const response = await fetch(url);
+    const genres = await response.json();
+  
+    res.json(genres);
+});
 
-
-module.exports = app;
+module.exports = router;
