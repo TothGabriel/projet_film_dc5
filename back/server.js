5be11acc262e03f1465f20ece2313bbcb76f42ef
Vilguax <axel.pelassa@hotmail.fr>
2023-12-21 14:21:43 +0100

    .

--- back/server.js
+++ back/server.js
@@ -22,3 +22,15 @@ app.use((req, res, next) => {
     console.log('Route reçue');
     next();
 });
+
+app.get('/categories', async (req, res) => {
+  const apiKey = process.env.API_KEY;
+  console.log(apiKey);
+  
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=fr-FR`;
+
+  const response = await fetch(url);
+  const genres = await response.json();
+
+  res.json(genres);
+});
