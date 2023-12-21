7dc3a9868b0b4c4fc2f0d73234ebac48ef9879c8
Vilguax <axel.pelassa@hotmail.fr>
2023-12-21 16:02:50 +0100

    .

--- back/routes/api/category.route.js
+++ back/routes/api/category.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+//const authMiddleware = require('../middleware/authMiddleware');
 
 /*router.get('/protected-route', authMiddleware, (req, res) => {
   res.send('Cette route est protégée et vous êtes authentifié.');
