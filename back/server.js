438dcf5d1f48bbbd6cde19966aae49d1ea0cef30
Vilguax <axel.pelassa@hotmail.fr>
2023-12-21 13:40:02 +0100

    update server.js

--- back/server.js
+++ back/server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.BACK_PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
