f3cf1113e92b0c1c10c90d5b67a23ed2aac08dab
Vilguax <axel.pelassa@hotmail.fr>
2024-04-17 14:20:40 +0200

    revue database

--- back/database.js
+++ back/database.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    const user = process.env.MONGODB_USER;
-    const pass = process.env.MONGODB_PASSWORD;
     await mongoose.connect(`mongodb+srv://axelpelassa:vFIZTdrO5MZ8ifBq@cluster0.zo2iqnr.mongodb.net/`);
     console.log('MongoDB connected');
   } catch (error) {
